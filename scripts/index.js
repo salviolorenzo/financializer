bf6756b91d4567a95a689debd893cf21e85157f5
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-28 13:30:48 -0400

    adds basic styles and html skeleton

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,4 @@
-// 
+// Purpose:
+// Top section of page displays general market information, including 'PANIC SELLING', general opinion on market volatility.
+// Top headlines for publicly traded companies affect the big 3
+// Search bar, search by key word or company, pulls up recent news, and stock information
